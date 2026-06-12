0b399d9b8b2650c9c1502316600d02ec3bc8ebee
2rlz <58435083+2rlz@users.noreply.github.com>
2026-06-12 15:44:18 +0900

    asdf
    asdf

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,19 +1,22 @@
 // Difficulty Table
 let mark = "";
 let data_link = "";
-const nowLang = navigator.language.slice(0, 2);
-const languagePrefix =
-  nowLang === "ko" ? "ko" : nowLang === "ja" ? "ja" : "en-GB";
-$(function () {
+let isSabunPage = window.location.href.indexOf("info");
+document.addEventListener("DOMContentLoaded", function () {
   async function getJSON() {
     const response = await fetch(
-      document.querySelector("meta[name=bmstable]").getAttribute("content")
+      document.querySelector("meta[name=bmstable]").getAttribute("content"),
     );
     const header = await response.json();
     document.getElementById("changelogText").value = "Loading...";
+    document.getElementById("smallTableTitle").innerHTML =
+      "<i class='fa-solid fa-table me-2'></i>" + header.name;
     if (header.symbol) mark = header.symbol;
     if (header.data_url) data_link = header.data_url;
-    if (header.enum_level_order) DataTable.enum(header.enum_level_order);
+    if (header.level_order) {
+      const enumOrder = header.level_order.map((e) => mark + e);
+      DataTable.enum(enumOrder);
+    }
     makeBMSTable();
   }
   if (document.querySelector("meta[name=bmstable]")) getJSON();
@@ -23,11 +26,23 @@ $(function () {
 function makeBMSTable() {
   let bmsTable = new DataTable("#tableDiff", {
     paging: false,
-    info: false,
-    lengthChange: false,
+
+    layout: {
+      bottomStart:
+        isSabunPage == -1
+          ? {
+              info: {
+                text:
+                  languagePrefix === "ko"
+                    ? "(검색된) 차분 갯수: _TOTAL_개"
+                    : "（検索された）差分数: _TOTAL_",
+              },
+            }
+          : null,
+    },
 
     language: {
-      url: `//cdn.datatables.net/plug-ins/2.3.3/i18n/${languagePrefix}.json`,
+      url: `//cdn.datatables.net/plug-ins/2.3.7/i18n/${languagePrefix}.json`,
     },
 
     ajax: {
@@ -40,18 +55,29 @@ function makeBMSTable() {
 
     createdRow: function (row, data) {
       const rowColor = {
-        1: "state1",
-        2: "state2",
-        3: "state3",
+        1: "bg-primary-subtle",
+        2: "bg-warning-subtle",
+        3: "bg-success-subtle",
+        4: "bg-secondary-subtle",
+        5: "bg-info-subtle",
       };
       if (data.state) row.classList.add(rowColor[data.state]);
     },
 
     initComplete: function () {
-      // Make Changelog
-      makeChangelog(bmsTable);
-      // Filter
-      makeFilter(bmsTable);
+      if (isSabunPage == -1) {
+        // Make Changelog
+        makeChangelog(bmsTable);
+        // Filter
+        makeFilter(bmsTable);
+        // Tooltips
+        const tooltipTriggerList = document.querySelectorAll(
+          "[data-bs-toggle='tooltip']",
+        );
+        const tooltipList = [...tooltipTriggerList].map(
+          (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl),
+        );
+      }
     },
   });
 }
@@ -61,11 +87,26 @@ function makeChangelog(table) {
   const data = table.ajax.json();
   // Special Date
   const blogOpen = {
-    date: "Wed Dec 04 2019 00:00:00 GMT+0900 (JST)",
-    title: "Site Open. 過去差分の修正本 Uploaded.",
+    date: "Sat Sep 08 2018 00:00:00 GMT+0900 (JST)",
+    title: "Sabun Blog Open.",
     state: "special",
   };
-  data.push(blogOpen);
+  const siteMove1 = {
+    date: "Thu Feb 28 2019 00:00:00 GMT+0900 (JST)",
+    title: "Site Moved.",
+    state: "special",
+  };
+  const siteMove2 = {
+    date: "Thu May 02 2019 00:00:00 GMT+0900 (JST)",
+    title: "Site Moved.",
+    state: "special",
+  };
+  const domainTransfer1 = {
+    date: "Fri Aug 15 2025 00:00:00 GMT+0900 (JST)",
+    title: "Domain Transferred. (bms.parksulab.xyz → parksu.darksabun.club)",
+    state: "special",
+  };
+  data.push(blogOpen, siteMove1, siteMove2, domainTransfer1);
   data.sort((a, b) => new Date(b.date) - new Date(a.date));
   const changelogData = data
     .map(function (song) {
@@ -73,7 +114,7 @@ function makeChangelog(table) {
       if (song.state == "special") {
         return `(${dateStr}) ${song.title}`;
       } else {
-        return `(${dateStr}) ${mark}${song.level} ${song.title}`;
+        return `(${dateStr}) ${mark}${song.level} ${song.title} Added.`;
       }
     })
     .join("\n");
@@ -84,16 +125,13 @@ function makeChangelog(table) {
 function makeFilter(table) {
   const column = table.column(0);
   const filterText =
-    languagePrefix === "ko"
-      ? "레벨별 필터: "
-      : languagePrefix === "ja"
-      ? "レベルでフィルタ: "
-      : "Filter by Level: ";
+    languagePrefix === "ko" ? "레벨별 필터: " : "レベルでフィルタ: ";
 
   const selectContainer = document.createElement("div");
   selectContainer.classList.add("dt-length");
 
   const select = document.createElement("select");
+  select.classList.add("form-select", "form-select-sm");
   select.add(new Option("All", ""));
 
   select.addEventListener("change", function () {
@@ -137,6 +175,12 @@ const tableData = {
     return mark + data;
   },
 
+  tableMochaIR: function (data) {
+    let MochaIrUrl = "https://mocha-repository.info/song.php?sha256=";
+    MochaIrUrl += data;
+    return `<a href='${MochaIrUrl}' target='_blank'>Mocha</a>`;
+  },
+
   tableTitle: function (data, type, row) {
     let MinIrUrl = `https://www.gaftalk.com/minir/#/viewer/song/${row.sha256}/0`;
     return `<a href='${MinIrUrl}' target='_blank'>${data}</a>`;
@@ -146,7 +190,9 @@ const tableData = {
     let scoreURL = "https://bms-score-viewer.pages.dev/view?md5=";
     scoreURL += data;
     if (data) {
-      return `<a href='${scoreURL}' target='_blank'>♪</a>`;
+      return `<a href='${scoreURL}' target='_blank'>
+                <i class='fa-solid fa-music fa-lg'></i>
+              </a>`;
     } else {
       return "";
     }
@@ -156,7 +202,9 @@ const tableData = {
     let movieURL = "https://www.youtube.com/watch?v=";
     if (data) {
       movieURL += data.slice(-11);
-      return `<a href='${movieURL}' target='_blank'>▶</a>`;
+      return `<a href='${movieURL}' target='_blank'>
+                <i class='fa-solid fa-play fa-lg'></i>
+              </a>`;
     } else {
       return "";
     }
@@ -193,7 +241,9 @@ const tableData = {
       if (data) {
         return `<a href='${row.url_diff}' target='_blank'>${data}</a>`;
       } else {
-        return `<a href='${row.url_diff}'>DL</a>`;
+        return `<a href='${row.url_diff}'>
+                  <i class='fa-solid fa-download fa-lg'></i>
+                </a>`;
       }
     } else {
       if (data) {
@@ -208,32 +258,94 @@ const tableData = {
     }
   },
 
+  // Total, Notes, Gauge
+  tableGauge: function (data, type, row) {
+    let gaugeCaculate, totalNotesStr, textColor, gaugeText, judgeRankText;
+
+    if (row.judge) {
+      const judgeRank = {
+        ve: "V.EASY<br />(if LR2, NORMAL)",
+        ez: "EASY",
+        nm: "NORMAL",
+        hd: "HARD",
+        vh: "V.HARD",
+        ran: "???",
+        none: "???<br />(if LR2, NORMAL)",
+      };
+      judgeRankText = "Judge Rank: " + judgeRank[row.judge];
+    }
+
+    if (row.notes === "ran" && row.total === "ran") {
+      gaugeCaculate = "???";
+      totalNotesStr = "Notes: ???<br />Total: ???<br />" + judgeRankText;
+    } else if (row.notes === "ran") {
+      gaugeCaculate = "???";
+      totalNotesStr =
+        "Notes: ???<br />Total: " + row.total + "<br />" + judgeRankText;
+    } else if (row.total === "0") {
+      const notes = Number(row.notes);
+
+      if (notes < 400) {
+        row.total = 0.16 * notes + 160;
+      } else if (notes >= 400 && notes < 600) {
+        row.total = 0.32 * notes + 96;
+      } else {
+        row.total = 0.16 * notes + 192;
+      }
+
+      gaugeCaculate = Math.floor((Number(row.total) / notes) * 1000) / 1000;
+      totalNotesStr = `Notes: ${row.notes}<br />Total: 0<br />${judgeRankText}<br />(Based on LR2 gauge calculation formula)`;
+    } else {
+      gaugeCaculate =
+        Math.floor((Number(row.total) / Number(row.notes)) * 1000) / 1000;
+      totalNotesStr = `Notes: ${row.notes}<br />Total: ${row.total}<br />${judgeRankText}`;
+    }
+
+    if (gaugeCaculate < 0.18) {
+      textColor = "text-danger";
+    } else if (gaugeCaculate > 0.25) {
+      textColor = "text-primary";
+    } else {
+      textColor = "";
+    }
+
+    gaugeText = `<span class='${textColor}'
+                    data-bs-toggle='tooltip'
+                    data-bs-html='true'
+                    title='${totalNotesStr}'>
+                      ${gaugeCaculate}
+                </span>`;
+
+    return gaugeText;
+  },
+
   // Comment
   tableComment: function (data, type, row) {
-    return row.comment || "";
+    if (getLanguage.slice(0, 2) === "ko") {
+      return row.comment_ko;
+    } else {
+      return row.comment;
+    }
   },
 };
 
 const defaultColumns = [
   {
     title: "Level",
-    width: "1%",
+    width: "5.75%",
     data: "level",
-    type: "natural",
     render: tableData.tableLevel,
   },
-
   {
-    title: "♪",
+    title: "<i class='fa-solid fa-music fa-lg'></i>",
     width: "1%",
     data: "md5",
     orderable: false,
     searchable: false,
     render: tableData.tableScore,
   },
-
   {
-    title: "▶",
+    title: "<i class='fa-solid fa-play fa-lg'></i>",
     width: "1%",
     data: "movie_link",
     orderable: false,
@@ -241,14 +353,22 @@ const defaultColumns = [
     render: tableData.tableMovie,
   },
   {
-    title: "Title<br />(LR2IR)",
+    title: "Mocha",
+    width: "1%",
+    data: "sha256",
+    orderable: false,
+    searchable: false,
+    render: tableData.tableMochaIR,
+  },
+  {
+    title: "Title<br />(MinIR)",
     width: "30%",
     data: "title",
     render: tableData.tableTitle,
   },
   {
     title: "Artist<br />(BMS DL)",
-    width: "25%",
+    width: "30%",
     data: "artist",
     render: tableData.tableArtist,
   },
@@ -256,20 +376,27 @@ const defaultColumns = [
     title: "DL",
     width: "1%",
     data: "name_diff",
-    className: "text-nowrap",
+    className: "text-nowrap one-percent-column",
+    orderable: false,
     render: tableData.tableChart,
   },
   {
     title: "Date",
-    width: "1%",
+    width: "5%",
     data: "date",
-    className: "text-nowrap",
+    className: "text-center five-percent-column font-monospace-custom",
     render: tableData.tableDate,
   },
   {
+    title: languagePrefix === "ko" ? "회복량<br />(T/N)" : "回復量<br />(T/N)",
+    width: "1%",
+    orderable: false,
+    className: "text-nowrap one-percent-column font-monospace-custom",
+    render: tableData.tableGauge,
+  },
+  {
     title: "Comment",
-    width: "25%",
+    width: "30%",
     render: tableData.tableComment,
   },
 ];
-
